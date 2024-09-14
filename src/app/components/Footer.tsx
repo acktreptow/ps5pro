6b3c60120815f5a2123715d366cfab64f9d9a62b
acktreptow <acktreptow@gmail.com>
2024-09-15 00:47:14 +0100

    add footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>PS5 Studios All Rights Reserved © 2024</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
