73368442c500ef22055e318f1099ef4979699624
acktreptow <acktreptow@gmail.com>
2024-09-16 15:58:28 +0100

    fill out header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,16 @@
 function Header() {
   return (
-    <header className="bg-gradient-to-b from-blue-700 to-playstation text-white uppercase flex items-center">
-      <h1>PS5 Pro</h1>
+    <header className="bg-gradient-to-b from-blue-700 to-playstation text-white uppercase flex items-center justify-between text-2xl p-10">
+      <div>
+        <h1>PS5 Pro</h1>
+      </div>
+      <div>
+        <ul className="flex space-x-10">
+          <li>About</li>
+          <li>Games List</li>
+          <li>Hardware Comparison</li>
+        </ul>
+      </div>
     </header>
   );
 }
