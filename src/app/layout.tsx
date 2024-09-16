a1b09264cc2a826f15b56ab12414d012970425d6
acktreptow <acktreptow@gmail.com>
2024-09-16 15:48:10 +0100

    add class names

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={``}>
+      <body className={`flex flex-col min-h-screen`}>
         <Header />
         {children}
         <Footer />
