45eaf24acefb9d8bd2c442192dcd7043e61a5b34
acktreptow <acktreptow@gmail.com>
2024-09-16 15:52:54 +0100

    style header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header>
+    <header className="bg-gradient-to-b from-blue-700 to-playstation text-white uppercase flex items-center">
       <h1>PS5 Pro</h1>
     </header>
   );
