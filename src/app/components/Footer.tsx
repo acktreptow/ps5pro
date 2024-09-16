51afc1e23bf63e4e92a47d8e6c4d36ac4bf020d1
acktreptow <acktreptow@gmail.com>
2024-09-16 15:51:19 +0100

    style footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="container">
+    <footer className="footer bg-gradient-to-b from-blue-700 to-playstation text-white text-center py-4 border-t-8 border-amber-300">
+      <div className="font-semibold">
         <p>PS5 Studios All Rights Reserved © 2024</p>
       </div>
     </footer>
