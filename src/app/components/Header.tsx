3b147b9b1ea8df0e41cd832e8294df73e11da47d
acktreptow <acktreptow@gmail.com>
2024-09-17 21:57:52 +0100

    create homepage link

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,9 @@ function Header() {
   return (
     <header className="bg-gradient-to-b from-blue-700 to-playstation text-white uppercase flex items-center justify-between text-2xl p-10">
       <div>
-        <h1>PS5 Pro</h1>
+        <Link href="/">
+          <h1>PS5 Pro</h1>
+        </Link>
       </div>
       <div>
         <ul className="flex space-x-10">
