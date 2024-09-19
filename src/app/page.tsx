987079ef875bc30e9395bfebf00e341bcfd53d05
acktreptow <acktreptow@gmail.com>
2024-09-19 23:55:51 +0100

    create form and options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,21 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import gamesData from "../data/games.json";
+import gamesData from "../app/data/games.json";
 
 function Home() {
   return (
     <div className="container mx-auto">
       <h1 className="text-6xl text-center py-10 font-bold">PS5 Pro</h1>
       <h2 className="text-4xl text-center py-10 font-semibold">Pick a game:</h2>
+      <form>
+        <select className="block mx-auto border-4 border-gray-500 rounded-lg p-2 mb-4">
+          <option>Select game</option>
+          {gamesData.map((game) => (
+            <option key={game.id}>{game.name}</option>
+          ))}
+        </select>
+      </form>
     </div>
   );
 }
