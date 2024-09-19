3a3f41335bc4e2cccab9bc9b76b0646fdce3c4fb
acktreptow <acktreptow@gmail.com>
2024-09-20 00:02:27 +0100

    update h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ function Home() {
   return (
     <div className="container mx-auto">
       <h1 className="text-6xl text-center py-10 font-bold">PS5 Pro</h1>
-      <h2 className="text-4xl text-center py-10 font-semibold">Pick a game:</h2>
+      <h2 className="text-4xl text-center py-10 font-semibold">
+        Everything you need to know about the Pro versions of PS5 games
+      </h2>
       <form>
         <select className="block mx-auto border-4 border-gray-500 rounded-lg p-2 mb-4">
           <option>Select game</option>
