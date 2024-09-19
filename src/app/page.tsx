b25db43dc8a0cb59499563f2dd6dd450de26a72a
acktreptow <acktreptow@gmail.com>
2024-09-20 00:33:00 +0100

    add star wars outlaws

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { useState } from "react";
+
 import Image from "next/image";
-import Link from "next/link";
 import gamesData from "../app/data/games.json";
 import ps5Pro from "../../public/images/PS5-Pro.jpg";
 
