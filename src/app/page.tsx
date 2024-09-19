47a4955a493ca3c7307c9bdfddc831ae6721fd79
acktreptow <acktreptow@gmail.com>
2024-09-19 23:49:52 +0100

    add imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import gamesData from "../data/games.json";
+
 function Home() {
   return (
     <div className="container mx-auto">
