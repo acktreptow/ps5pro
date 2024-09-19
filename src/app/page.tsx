6e054e911e84af7267096eb09906283ce38af03d
acktreptow <acktreptow@gmail.com>
2024-09-19 23:48:04 +0100

    add titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div></div>;
+function Home() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-6xl text-center py-10 font-bold">PS5 Pro</h1>
+      <h2 className="text-4xl text-center py-10 font-semibold">Pick a game:</h2>
+    </div>
+  );
 }
+
+export default Home;
