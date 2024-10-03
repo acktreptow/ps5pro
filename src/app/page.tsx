1d8ce492031c08a29cf3f5ce505b9a56d8922219
acktreptow <acktreptow@gmail.com>
2024-10-03 20:32:02 +0100

    add form functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,32 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
-import gamesData from "../app/data/games.json";
+import gamesData from "../app/data/postableGames.json";
 import ps5Pro from "../../public/images/PS5-Pro.jpg";
 
 function Home() {
+  const [selectedGame, setSelectedGame] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (selectedGame) {
+      window.location.href = `/games/${selectedGame}`;
+    }
+  }
+
   return (
     <div className="container mx-auto flex-grow">
       <h1 className="text-6xl text-center py-10 font-bold">PS5 Pro</h1>
       <h2 className="text-4xl text-center py-10 font-semibold">
         Everything you need to know about the Pro versions of PS5 games
       </h2>
-      <form>
-        <select className="block mx-auto border-4 border-gray-500 rounded-lg p-2 mb-4">
+      <form onSubmit={handleSubmit}>
+        <select
+          value={selectedGame}
+          onChange={(e) => setSelectedGame(e.target.value)}
+          className="block mx-auto border-4 border-gray-500 rounded-lg p-2 mb-4"
+        >
           <option>Select game</option>
           {gamesData.map((game) => (
             <option key={game.id}>{game.name}</option>
