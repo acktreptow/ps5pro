bcd57ed597f04a8a04ea0d6217df65d82ad54906
acktreptow <acktreptow@gmail.com>
2024-10-12 06:56:36 +0100

    reorder content

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,9 @@ export const Footer = (): JSX.Element => {
     <footer className="text-slate-50 text-center font-semibold py-4 bg-gradient-to-b from-blue-700 to-playstation border-t-8 border-slate-900 lg:text-lg">
       <p>
         PS5 Pro Enhanced Games <span className="hidden sm:inline">•</span>{" "}
-        <span className="block sm:inline">All Rights Reserved © 2024 • Alex Treptow</span>
+        <span className="block sm:inline">
+          © 2024 Alex Treptow • All Rights Reserved
+        </span>
       </p>
     </footer>
   );
