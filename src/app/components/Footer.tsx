15989aef479251e0a293d66198426bff50210545
acktreptow <acktreptow@gmail.com>
2024-10-12 06:46:56 +0100

    add breakpoint classes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,10 @@
 export const Footer = (): JSX.Element => {
   return (
-    <footer className="text-slate-50 text-center font-semibold py-4 bg-gradient-to-b from-blue-700 to-playstation border-t-8 border-slate-900">
-      <p>PS5 Pro Enhanced Games • All Rights Reserved © 2024 • Alex Treptow</p>
+    <footer className="text-slate-50 text-center font-semibold py-4 bg-gradient-to-b from-blue-700 to-playstation border-t-8 border-slate-900 lg:text-lg">
+      <p>
+        PS5 Pro Enhanced Games <span className="hidden sm:inline">•</span>{" "}
+        <span className="block sm:inline">All Rights Reserved © 2024 • Alex Treptow</span>
+      </p>
     </footer>
   );
 };
