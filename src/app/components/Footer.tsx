9cf9481e68cf0e3b265c912a1d8b67f25b4d94d6
acktreptow <acktreptow@gmail.com>
2024-10-12 05:40:26 +0100

    update text & border colors

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,7 @@
 export const Footer = (): JSX.Element => {
   return (
-    <footer className="footer bg-gradient-to-b from-blue-700 to-playstation text-white text-center py-4 border-t-8 border-amber-300">
-      <div className="font-semibold">
-        <p>
-          PS5 Pro Enhanced Games • All Rights Reserved © 2024 • Alex Treptow
-        </p>
-      </div>
+    <footer className="text-slate-50 text-center font-semibold py-4 bg-gradient-to-b from-blue-700 to-playstation border-t-8 border-slate-900">
+      <p>PS5 Pro Enhanced Games • All Rights Reserved © 2024 • Alex Treptow</p>
     </footer>
   );
 };
