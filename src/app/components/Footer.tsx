419d16bb67ae837d2325a0186f7b34c5d6b59f55
acktreptow <acktreptow@gmail.com>
2024-10-12 05:22:51 +0100

    update footer contents and add type safety

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,10 @@
-export const Footer = () => {
+export const Footer = (): JSX.Element => {
   return (
     <footer className="footer bg-gradient-to-b from-blue-700 to-playstation text-white text-center py-4 border-t-8 border-amber-300">
       <div className="font-semibold">
-        <p>PS5 Studios All Rights Reserved © 2024</p>
+        <p>
+          PS5 Pro Enhanced Games • All Rights Reserved © 2024 • Alex Treptow
+        </p>
       </div>
     </footer>
   );
