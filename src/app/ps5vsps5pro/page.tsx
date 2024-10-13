c6c826a42b0918fdd06d2555619c60eac788c0fc
acktreptow <acktreptow@gmail.com>
2024-10-13 17:46:19 +0100

    update h1 classes

--- src/app/ps5vsps5pro/page.tsx
+++ src/app/ps5vsps5pro/page.tsx
@@ -1,10 +1,10 @@
 const PS5vsPS5ProPage = (): JSX.Element => {
   return (
-    <div className="container mx-auto space-y-20 flex-grow">
-      <h1 className="text-center text-2xl font-extrabold m-10">
-        Hardware comparison
+    <div className="container mx-auto flex-grow">
+      <h1 className="text-slate-800 text-center text-6xl font-extrabold my-16 bg-slate-100 p-8 rounded shadow-lg">
+        PS5 Vs PS5 Pro
       </h1>
-      <p>This is the hardware comparison page</p>
+      <h2>Specifications</h2>
     </div>
   );
 };
