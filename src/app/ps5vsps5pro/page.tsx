9dba43edb353cfd7a4469830646dd8bc9e140216
acktreptow <acktreptow@gmail.com>
2024-10-13 17:55:58 +0100

    lower values

--- src/app/ps5vsps5pro/page.tsx
+++ src/app/ps5vsps5pro/page.tsx
@@ -1,11 +1,11 @@
 const PS5vsPS5ProPage = (): JSX.Element => {
   return (
     <div className="container mx-auto flex-grow">
-      <h1 className="text-slate-800 text-center text-6xl font-extrabold my-16 bg-slate-100 p-8 rounded shadow-lg">
+      <h1 className="text-slate-800 text-center text-4xl font-extrabold my-8 bg-slate-100 p-8 rounded shadow-lg">
         PS5 Vs PS5 Pro
       </h1>
       <div className="bg-slate-100 p-8 rounded shadow-lg">
-        <h2 className="text-slate-800 text-4xl font-extrabold">
+        <h2 className="text-slate-800 text-2xl font-bold">
           Specifications
         </h2>
       </div>
