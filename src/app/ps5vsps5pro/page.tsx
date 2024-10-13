fbb5fced74bf560528f80c96267f780ed77068e6
acktreptow <acktreptow@gmail.com>
2024-10-13 17:52:48 +0100

    add bg div

--- src/app/ps5vsps5pro/page.tsx
+++ src/app/ps5vsps5pro/page.tsx
@@ -4,7 +4,11 @@ const PS5vsPS5ProPage = (): JSX.Element => {
       <h1 className="text-slate-800 text-center text-6xl font-extrabold my-16 bg-slate-100 p-8 rounded shadow-lg">
         PS5 Vs PS5 Pro
       </h1>
-      <h2>Specifications</h2>
+      <div className="bg-slate-100 p-8 rounded shadow-lg">
+        <h2 className="text-slate-800 text-4xl font-extrabold">
+          Specifications
+        </h2>
+      </div>
     </div>
   );
 };
