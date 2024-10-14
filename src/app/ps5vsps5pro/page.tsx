00466c00e179d4549884f43e506ca4b56005a362
acktreptow <acktreptow@gmail.com>
2024-10-14 03:35:28 +0100

    add gpu info

--- src/app/ps5vsps5pro/page.tsx
+++ src/app/ps5vsps5pro/page.tsx
@@ -11,9 +11,11 @@ const PS5vsPS5ProPage = (): JSX.Element => {
         <div className="flex justify-around">
           <div>
             <h3 className="text-slate-800 text-xl font-bold mb-8">PS5</h3>
+            <p>GPU: Custom AMD RDNA 2 | 36 CUs up to 2.23 GHz | 10.28 TFLOPs</p>
           </div>
           <div>
             <h3 className="text-slate-800 text-xl font-bold mb-8">PS5 Pro</h3>
+            <p>GPU: Custom AMD RDNA 3 | 60 CUs up to 2.23 GHz | 17.13 TFLOPs</p>
           </div>
         </div>
       </div>
