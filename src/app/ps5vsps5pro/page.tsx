a00c39cfd463f55a8b6e9609875f94bf7baee0b3
acktreptow <acktreptow@gmail.com>
2024-10-14 04:54:59 +0100

    add ai/ml info

--- src/app/ps5vsps5pro/page.tsx
+++ src/app/ps5vsps5pro/page.tsx
@@ -18,13 +18,18 @@ const PS5vsPS5ProPage = (): JSX.Element => {
             </p>
             <p>RAM: 16GB GDDR6 memory @ 14 Gbps</p>
             <p>Memory available for games: 12.5 GB</p>
-            <p>Storage: 825GB (1TB for Slim model) NVME SSD @ 5.5GB/s read</p>
             <p>Memory Interface/Bandwidth: 256-bit / 448GB/s</p>
+            <p>AI/Machine Learning: N/A</p>
+            <p>Storage: 825GB (1TB for Slim model) NVME SSD @ 5.5GB/s read</p>
             <p>Output: Up to 4K 120Hz | HDMI 2.1</p>
             <p>WiFi: WiFi 6</p>
             <p>Dimensions: H: 9.6cm | W: 35.8cm (Slim)</p>
             <p>
               Launch Date: November 12, 2020 (NA/JP/AU) | November 19, 2020 (WW)
+            </p>
+            <p>
+              Introductory Price: Disc Model - $499 / €499 / £449 / ¥49,980 |
+              Digital Model - $399 / €399 / £359 / ¥39,980
             </p>
           </div>
           <div>
@@ -37,11 +42,20 @@ const PS5vsPS5ProPage = (): JSX.Element => {
             <p>RAM: 16GB GDDR6 memory @ 18 Gbps</p>
             <p>Memory available for games: 13.7 GB</p>
             <p>Memory Interface/Bandwidth: 256-bit / 576GB/s</p>
-            <p>Output: Up to 4K 120Hz or 8K 60hz | HDMI 2.1</p>
+            <p>
+              AI/Machine Learning: 300 TOPS of 8-bit computation / 67 TFLOPS of
+              16-bit floating point.
+            </p>
+            <p>
+              AI/Machine Learning Features: PSSR (PlayStation Spectral Super
+              Resolution Upscaling) & 2-4x Ray-tracing.
+            </p>
             <p>Storage: 2TB NVME SSD @ 5.5GB/s read</p>
+            <p>Output: Up to 4K 120Hz or 8K 60hz | HDMI 2.1</p>
             <p>WiFi: WiFi 7</p>
             <p>Dimensions: H: 10.4cm | W: 35.8cm</p>
             <p>Launch Date: November 7, 2024 (WW)</p>
+            <p>Introductory Price: $699 / €799 / £699 / ¥119,980</p>
           </div>
         </div>
       </div>
