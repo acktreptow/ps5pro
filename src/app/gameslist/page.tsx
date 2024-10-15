3d2aff494c20b8ea82a324baa81c8ffe2d0cd0a0
acktreptow <acktreptow@gmail.com>
2024-10-16 00:51:34 +0100

    add h2 tag

--- src/app/gameslist/page.tsx
+++ src/app/gameslist/page.tsx
@@ -4,6 +4,11 @@ const GamesPage = (): JSX.Element => {
       <h1 className="text-slate-800 text-center text-4xl font-extrabold my-8 bg-slate-100 p-8 rounded shadow-lg">
         All PS5 Pro Enhanced Games
       </h1>
+      <div className="bg-slate-100 p-8 rounded shadow-lg mb-8">
+        <h2 className="text-slate-800 text-2xl font-bold mb-4 underline">
+          Confirmed Games
+        </h2>
+      </div>
     </div>
   );
 };
