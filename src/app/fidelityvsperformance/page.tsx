4482ca3419f69a8af5b38146099646ee14e02aed
acktreptow <acktreptow@gmail.com>
2024-10-16 20:51:16 +0100

    add h3 headers

--- src/app/fidelityvsperformance/page.tsx
+++ src/app/fidelityvsperformance/page.tsx
@@ -12,8 +12,50 @@ const PS5vsPS5ProPage = (): JSX.Element => {
           There are many factors to consider when choosing the difference as
           well as more indirect things to look for. Here they are:
         </p>
+        <div>
+          <h3 className="text-slate-800 text-xl font-bold mb-4 underline">
+            Game factors
+          </h3>
+          <ul className="mb-8">
+            <li>Is the game first or third-person?</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-slate-800 text-xl font-bold mb-4 underline">
+            Option menu factors
+          </h3>
+          <ul className="mb-8">
+            <li>Does the game use motion blur?</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-slate-800 text-xl font-bold mb-4 underline">
+            Screen factors
+          </h3>
+          <ul className="mb-8">
+            <li>What type of screen is being used?</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-slate-800 text-xl font-bold mb-4 underline">
+            External factors
+          </h3>
+          <ul className="mb-8">
+            <li>How close are you to the screen?</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-slate-800 text-xl font-bold mb-4 underline">
+            Personal factors
+          </h3>
+          <ul className="mb-8">
+            <li>
+              Can you personally feel a big difference between 30 and 60fps?
+            </li>
+          </ul>
+        </div>
+
         <ul>
-          <li>Is the game first or third-person?</li>
           <li>How fast is the game?</li>
           <li>Is the game a shooter?</li>
           <li>What genre is the game?</li>
@@ -34,7 +76,7 @@ const PS5vsPS5ProPage = (): JSX.Element => {
           <li>
             Is the singleplayer game emphasizing graphics as a selling point?
           </li>
-          <li>What type of screen is being used?</li>
+
           <li>What resolution is the screen?</li>
           <li>What size of the screen is being used?</li>
           <li>What is the refresh rate of the screen?</li>
@@ -43,14 +85,12 @@ const PS5vsPS5ProPage = (): JSX.Element => {
             Does the screen support a good Game mode/have a low response time?
           </li>
           <li>Is the room the game being played in a dark or bright room?</li>
-          <li>How close are you to the screen?</li>
           <li>Does the game support good HDR?</li>
           <li>Does the game even support HDR?</li>
           <li>Does the game have a weird filter?</li>
           <li>Is the game a remake or remaster?</li>
           <li>How dense is the geometry?</li>
           <li>How detailed are the textures?</li>
-          <li>Does the game use motion blur?</li>
           <li>Does the game use chromatic aberration?</li>
           <li>Does the game use depth of field?</li>
           <li>Does the game use film grain?</li>
@@ -73,9 +113,6 @@ const PS5vsPS5ProPage = (): JSX.Element => {
           </li>
           <li>How long are your gaming sessions?</li>
           <li>Does the game use photo mode and do you use it a lot?</li>
-          <li>
-            Can you personally feel a big difference between 30 and 60fps?
-          </li>
           <li>Can you personally see a big difference between resolutions?</li>
         </ul>
       </div>
