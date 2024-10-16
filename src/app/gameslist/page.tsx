da2bcab115439eb9076d039a22f2ff49d87094dd
acktreptow <acktreptow@gmail.com>
2024-10-16 01:09:38 +0100

    convert to h3 with classes

--- src/app/gameslist/page.tsx
+++ src/app/gameslist/page.tsx
@@ -7,36 +7,36 @@ const GamesPage = (): JSX.Element => {
         All PS5 Pro Enhanced Games
       </h1>
       <div className="bg-slate-100 p-8 rounded shadow-lg mb-8">
-        <h2 className="text-slate-800 text-2xl font-bold mb-4 underline">
+        <h2 className="text-slate-800 text-2xl font-bold mb-8 underline">
           Confirmed Games
         </h2>
         <div className="flex justify-between">
           <div>
-            <h2>Title</h2>
+            <h3 className="text-slate-800 text-xl font-bold mb-4 underline">Title</h3>
             {confirmedGames.map((game) => (
               <p key={game.id}>{game.name}</p>
             ))}
           </div>
           <div>
-            <h2>Genre</h2>
+            <h3 className="text-slate-800 text-xl font-bold mb-4 underline">Genre</h3>
             {confirmedGames.map((game) => (
               <p key={game.id}>{game.genre}</p>
             ))}
           </div>
           <div>
-            <h2>Developer</h2>
+            <h3 className="text-slate-800 text-xl font-bold mb-4 underline">Developer</h3>
             {confirmedGames.map((game) => (
               <p key={game.id}>{game.developer}</p>
             ))}
           </div>
           <div>
-            <h2>Release Date</h2>
+            <h3 className="text-slate-800 text-xl font-bold mb-4 underline">Release Date</h3>
             {confirmedGames.map((game) => (
               <p key={game.id}>{game.release}</p>
             ))}
           </div>
           <div>
-            <h2>Metacritic</h2>
+            <h3 className="text-slate-800 text-xl font-bold mb-4 underline">Metacritic</h3>
             {confirmedGames.map((game) => (
               <p key={game.id}>{game.metacritic}</p>
             ))}
