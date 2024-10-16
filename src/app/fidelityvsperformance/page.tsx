8d5a191393c0b78de4d63f79eee594d6c4449433
acktreptow <acktreptow@gmail.com>
2024-10-16 19:10:25 +0100

    start list

--- src/app/fidelityvsperformance/page.tsx
+++ src/app/fidelityvsperformance/page.tsx
@@ -8,6 +8,17 @@ const PS5vsPS5ProPage = (): JSX.Element => {
         <h2 className="text-slate-800 text-2xl font-bold mb-8 underline">
           Why the choice is a game by game choice
         </h2>
+        <p className="mb-4 text-xl">
+          There are many factors to consider when choosing the difference as
+          well as more indirect things to look for. Here they are:
+        </p>
+        <ul>
+          <li>Is the game first or third-person?</li>
+          <li>What genre is the game?</li>
+          <li>Does the game have an FOV slider?</li>
+          <li>Does the game hold a steady 60fps?</li>
+          <li>If not, does it at least stay above the VRR threshold?</li>
+        </ul>
       </div>
     </div>
   );
