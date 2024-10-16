7c8fe282eef319de7d80a1c145c5f58afc0277d6
acktreptow <acktreptow@gmail.com>
2024-10-16 01:02:19 +0100

    add columns

--- src/app/gameslist/page.tsx
+++ src/app/gameslist/page.tsx
@@ -1,3 +1,5 @@
+import confirmedGames from "../data/confirmedGames.json";
+
 const GamesPage = (): JSX.Element => {
   return (
     <div className="container mx-auto flex-grow text-slate-800">
@@ -8,6 +10,23 @@ const GamesPage = (): JSX.Element => {
         <h2 className="text-slate-800 text-2xl font-bold mb-4 underline">
           Confirmed Games
         </h2>
+        <div className="flex">
+          <div>
+            <h2>Title</h2>
+          </div>
+          <div>
+            <h2>Genre</h2>
+          </div>
+          <div>
+            <h2>Developer</h2>
+          </div>
+          <div>
+            <h2>Release Date</h2>
+          </div>
+          <div>
+            <h2>Metacritic</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
