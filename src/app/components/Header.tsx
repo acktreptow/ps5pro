e2ddbe77fd7c111bb7e30c585d9d717cdfe37c75
acktreptow <acktreptow@gmail.com>
2024-10-17 05:21:55 +0100

    add tailwind breakpoint classes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,20 +3,20 @@ import Image from "next/image";
 
 export const Header = (): JSX.Element => {
   return (
-    <header className="text-slate-50 text-3xl py-8 uppercase font-semibold bg-gradient-to-b from-blue-700 to-playstation border-b-8 border-slate-900 flex flex-col gap-8 items-center">
+    <header className="text-slate-50 uppercase font-semibold py-8 flex flex-col gap-8 items-center bg-gradient-to-b from-blue-700 to-playstation border-b-8 border-slate-900 md:text-lg lg:text-2xl">
       <div>
         <Link href="/">
           <Image
             src="/images/ps5-pro-logo.png"
             alt="PS5 Pro Logo"
-            width={250}
+            width={200}
             height={50}
-            className="rounded-lg border-2 border-slate-50"
+            className="rounded-lg border-2 border-slate-50 md:w-56 lg:w-64"
           />
         </Link>
       </div>
       <div>
-        <ul className="flex space-x-10">
+        <ul className="flex flex-col items-center gap-1 md:flex-row md:space-x-4 lg:space-x-6">
           <li className="hover:text-slate-300 hover:underline">
             <Link href="/">Home</Link>
           </li>
@@ -30,7 +30,7 @@ export const Header = (): JSX.Element => {
             <Link href="/ps5vsps5pro">PS5 vs PS5 Pro</Link>
           </li>
           <li className="hover:text-slate-300 hover:underline">
-            <Link href="/fidelityvsperformance">Fidelity vs Performance</Link>
+            <Link href="/fidelityvsperformance">Graphics vs Performance</Link>
           </li>
         </ul>
       </div>
