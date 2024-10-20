96cabc7d17a3214256cfaa1c985eb696c9d9f07f
acktreptow <acktreptow@gmail.com>
2024-10-20 02:53:15 +0100

    remove color classes

--- src/app/gameslist/page.tsx
+++ src/app/gameslist/page.tsx
@@ -7,46 +7,34 @@ const GamesPage = (): JSX.Element => {
         All PS5 Pro Enhanced Games
       </h1>
       <div className="mx-4 mb-10 p-4 pb-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8 md:p-8 lg:p-12">
-        <h2 className="text-slate-800 text-2xl font-bold mb-8 underline">
-          Confirmed Games
-        </h2>
+        <h2 className="text-2xl font-bold mb-8 underline">Confirmed Games</h2>
         <div className="flex justify-between mb-8">
           <div>
-            <h3 className="text-slate-800 text-xl font-bold mb-4 underline">
-              Title
-            </h3>
+            <h3 className="text-xl font-bold mb-4 underline">Title</h3>
             {confirmedGames.map((game) => (
               <p key={game.id}>{game.name}</p>
             ))}
           </div>
           <div>
-            <h3 className="text-slate-800 text-xl font-bold mb-4 underline">
-              Genre
-            </h3>
+            <h3 className="text-xl font-bold mb-4 underline">Genre</h3>
             {confirmedGames.map((game) => (
               <p key={game.id}>{game.genre}</p>
             ))}
           </div>
           <div>
-            <h3 className="text-slate-800 text-xl font-bold mb-4 underline">
-              Developer
-            </h3>
+            <h3 className="text-xl font-bold mb-4 underline">Developer</h3>
             {confirmedGames.map((game) => (
               <p key={game.id}>{game.developer}</p>
             ))}
           </div>
           <div>
-            <h3 className="text-slate-800 text-xl font-bold mb-4 underline">
-              Release Date
-            </h3>
+            <h3 className="text-xl font-bold mb-4 underline">Release Date</h3>
             {confirmedGames.map((game) => (
               <p key={game.id}>{game.release}</p>
             ))}
           </div>
           <div>
-            <h3 className="text-slate-800 text-xl font-bold mb-4 underline">
-              Metacritic
-            </h3>
+            <h3 className="text-xl font-bold mb-4 underline">Metacritic</h3>
             {confirmedGames.map((game) => (
               <p key={game.id}>{game.metacritic}</p>
             ))}
