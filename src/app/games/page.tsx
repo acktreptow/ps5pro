34b9bd58fe718636dfa25ae946c23d3a136b36d3
acktreptow <acktreptow@gmail.com>
2024-10-26 03:55:42 +0100

    convert to server component

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,25 +1,8 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-const GamesPage = (): JSX.Element => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/gameslist/");
-  }, [router]);
-
-  return <div></div>;
+const GamesPage = () => {
+  redirect("/gameslist/");
+  return null;
 };
 
 export default GamesPage;
-
-// import { redirect } from "next/navigation";
-
-// const GamesPage = () => {
-//   redirect("/gameslist/");
-//   return null;
-// };
-
-// export default GamesPage;
