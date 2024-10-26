d7c440f8f033a85a53e1fdd32251e8033a6d7893
acktreptow <acktreptow@gmail.com>
2024-10-26 03:50:53 +0100

    add both ways

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 const GamesPage = (): JSX.Element => {
-  useRouter().push("/gameslist/");
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/gameslist/");
+  }, [router]);
 
   return <div></div>;
 };
 
 export default GamesPage;
+
+// import { redirect } from "next/navigation";
+
+// const GamesPage = () => {
+//   redirect("/gameslist/");
+//   return null;
+// };
+
+// export default GamesPage;
