ec8406f12583a9c1502118a1ece961d5738ee7b1
acktreptow <acktreptow@gmail.com>
2024-10-27 03:21:41 +0000

    import games

--- src/app/games/[game]/page.tsx
+++ src/app/games/[game]/page.tsx
@@ -1,9 +1,16 @@
+import postableGames from "../../data/postableGames.json";
+
 const GamesPage = () => {
   return (
     <div className="container mx-auto flex-grow text-slate-800">
       <h1 className="text-center text-4xl font-extrabold mt-8 mb-12 mx-4 p-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8">
         PS5 Pro Game X
       </h1>
+      <div className="mx-4 mb-10 p-4 pb-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8 md:p-8 lg:p-12">
+        <h2 className="text-3xl text-center font-bold underline my-4">
+          Hello World
+        </h2>
+      </div>
     </div>
   );
 };
