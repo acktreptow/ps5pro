774a17a39b0911511833533fa6932db68cc4680e
acktreptow <acktreptow@gmail.com>
2024-10-27 03:25:38 +0000

    create game variable

--- src/app/games/[game]/page.tsx
+++ src/app/games/[game]/page.tsx
@@ -1,6 +1,8 @@
 import postableGames from "../../data/postableGames.json";
 
-const GamesPage = () => {
+const GamesPage = ({ params }) => {
+  const game = postableGames.find((game) => game.urlPath === params.game);
+
   return (
     <div className="container mx-auto flex-grow text-slate-800">
       <h1 className="text-center text-4xl font-extrabold mt-8 mb-12 mx-4 p-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8">
@@ -8,7 +10,7 @@ const GamesPage = () => {
       </h1>
       <div className="mx-4 mb-10 p-4 pb-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8 md:p-8 lg:p-12">
         <h2 className="text-3xl text-center font-bold underline my-4">
-          Hello World
+          {game.name}
         </h2>
       </div>
     </div>
