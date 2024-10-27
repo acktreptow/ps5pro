f0e24bedbf9107e5458283fc6134d57f60ee83aa
acktreptow <acktreptow@gmail.com>
2024-10-27 04:22:38 +0000

    add enhancements details

--- src/app/games/[game]/page.tsx
+++ src/app/games/[game]/page.tsx
@@ -51,7 +51,7 @@ const GamesPage: React.FC<GamesPageProps> = ({ params }) => {
       </h1>
       <div className="mx-4 mb-10 p-4 pb-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8 md:p-8 lg:p-12">
         <h2 className="text-3xl text-center font-bold underline my-4">
-          Details
+          Game Details
         </h2>
         <p>
           <span className="font-semibold">Genre:</span> {game.genre}
@@ -63,9 +63,50 @@ const GamesPage: React.FC<GamesPageProps> = ({ params }) => {
           <span className="font-semibold">Release Date:</span> {game.release}
         </p>
         <p>
-          <span className="font-semibold">PS Studios Game?:</span>{" "}
+          <span className="font-semibold">PS Studios Game?</span>{" "}
           {game.psStudios ? "Yes" : "No"}
         </p>
+        <p>
+          <span className="font-semibold">Metacritic Score:</span>{" "}
+          {game.metacritic}
+        </p>
+        <h2 className="text-3xl text-center font-bold underline my-4">
+          Enhancements
+        </h2>
+        <p>
+          <span className="font-semibold">PSSR?</span>{" "}
+          {game.pssr ? "Yes" : "No"}
+        </p>
+        <p>
+          <span className="font-semibold">RT?</span> {game.rt ? "Yes" : "No"}
+        </p>
+        {game.rt && (
+          <p>
+            <span className="font-semibold">RT Types:</span> {game.rtTypes}
+          </p>
+        )}
+        <p>
+          <span className="font-semibold">Highest Resolution:</span>{" "}
+          {game.resolution}{" "}
+        </p>
+        <p>
+          <span className="font-semibold">Highest FPS:</span> {game.fps}{" "}
+        </p>
+        {game.modes && (
+          <div>
+            <h2 className="text-3xl text-center font-bold underline my-4">
+              Modes
+            </h2>
+            <p>
+              <span className="font-semibold">Fidelity:</span>{" "}
+              {game.modes.fidelity}
+            </p>
+            <p>
+              <span className="font-semibold">Performance:</span>{" "}
+              {game.modes.performance}
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
