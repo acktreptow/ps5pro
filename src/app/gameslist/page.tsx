42b20b78c78fbeedc94e42a28f6e3a948ecd260a
acktreptow <acktreptow@gmail.com>
2024-10-27 03:10:58 +0000

    update component name

--- src/app/gameslist/page.tsx
+++ src/app/gameslist/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
     "A list of all PS5 Pro enhanced games, detailing the genre, developer, release date, and Metacritic score.",
 };
 
-const GamesPage = (): JSX.Element => {
+const GamesListPage = (): JSX.Element => {
   return (
     <div className="container mx-auto flex-grow text-slate-800">
       <h1 className="text-center text-4xl font-extrabold mt-8 mb-12 mx-4 p-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8">
@@ -90,4 +90,4 @@ const GamesPage = (): JSX.Element => {
   );
 };
 
-export default GamesPage;
+export default GamesListPage;
