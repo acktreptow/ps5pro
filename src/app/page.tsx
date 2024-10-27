683fd69d8d5d2255e4aaca1e7e860c35e581ce9e
acktreptow <acktreptow@gmail.com>
2024-10-27 16:58:29 +0000

    update imports and classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import gamesData from "../app/data/postableGames.json";
+import games from "../app/data/postableGames.json";
 import ps5Pro from "../../public/images/PS5-Pro.jpg";
 
 function Home() {
@@ -26,13 +26,13 @@ function Home() {
         <h2 className="text-center text-2xl font-extrabold mt-4 mb-12 md:mt-0 lg:text-3xl">
           Detailing how your favorite PS5 games are enhanced for the PS5 Pro:
         </h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="text-lg">
           <select
             onChange={(e) => setSelectedGame(e.target.value)}
-            className="block mx-auto border-4 border-gray-500 rounded-lg p-2 mb-4 text-lg "
+            className="block mx-auto border-2 border-slate-800 rounded-lg p-2 mb-4"
           >
             <option>Select PS5 Pro Game</option>
-            {gamesData.map((game) => (
+            {games.map((game) => (
               <option key={game.id} value={game.urlPath}>
                 {game.name}
               </option>
@@ -40,7 +40,7 @@ function Home() {
           </select>
           <button
             type="submit"
-            className="block mx-auto text-lg border font-bold p-4 mb-12 bg-gradient-to-b from-blue-700 to-playstation text-white rounded-full hover:bg-gradient-to-b hover:from-blue-600 hover:to-playstation  transition duration-300 shadow-md shadow-playstation hover:scale-105"
+            className="text-slate-100 block mx-auto border font-bold p-4 mb-12 bg-gradient-to-b from-blue-700 to-playstation rounded-full shadow-md shadow-playstation hover:bg-gradient-to-b hover:from-blue-600 hover:to-playstation transition duration-300 hover:scale-105"
           >
             Review Pro Enhancements
           </button>
