95240b6d258464b2f38d9c18f4a2133e93fe29a0
acktreptow <acktreptow@gmail.com>
2024-10-27 03:13:20 +0000

    create boiler plate template code

---
+++ src/app/games/[game]/page.tsx
@@ -0,0 +1,11 @@
+const GamesPage = () => {
+  return (
+    <div className="container mx-auto flex-grow text-slate-800">
+      <h1 className="text-center text-4xl font-extrabold mt-8 mb-12 mx-4 p-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8">
+        PS5 Pro Game X
+      </h1>
+    </div>
+  );
+};
+
+export default GamesPage;
