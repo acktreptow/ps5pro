cc52344aeb25d5d49fa52df7e1684dd1a09dd215
acktreptow <acktreptow@gmail.com>
2024-10-30 05:05:41 +0000

    create and connect postgres db with prisma

--- src/app/gameslist/page.tsx
+++ src/app/gameslist/page.tsx
@@ -2,6 +2,7 @@ import nonDetailedGames from "../data/nonDetailedGames.json";
 import postableGames from "../data/postableGames.json";
 import type { Metadata } from "next";
 import Link from "next/link";
+import prisma from "../../../lib/prisma";
 
 export const metadata: Metadata = {
   title: "Games List",
@@ -9,12 +10,57 @@ export const metadata: Metadata = {
     "A list of all PS5 Pro enhanced games, detailing the genre, developer, release date, and Metacritic score.",
 };
 
-const GamesListPage = (): JSX.Element => {
+async function getConfirmedGames() {
+  const confirmedGames = await prisma.confirmedGame.findMany({
+    where: { published: true },
+    select: {
+      id: true,
+      title: true,
+      genre: true,
+      developer: true,
+      psStudios: true,
+      release: true,
+      metacritic: true,
+    },
+  });
+  return confirmedGames;
+}
+
+const GamesListPage = async () => {
+  const confirmedGames = await getConfirmedGames();
   return (
     <div className="container mx-auto flex-grow text-slate-800">
       <h1 className="text-center text-4xl font-extrabold mt-8 mb-12 mx-4 p-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8">
         All PS5 Pro Enhanced Games
       </h1>
+      <h2 className="text-3xl text-center font-bold underline mb-4 lg:text-start lg:mx-8">
+        Confirmed Games
+      </h2>
+      <div className="text-xl text-center mb-8 mx-4 grid gap-5 md:text-lg md:mx-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-4">
+        {confirmedGames.map((game) => (
+          <div
+            className="py-6 px-4 bg-slate-100 border-2 rounded shadow-md lg:px-2"
+            key={game.id}
+          >
+            <h3 className="font-extrabold text-2xl mb-2 md:text-xl lg:mb-4">
+              {game.title}
+            </h3>
+            <p>
+              <span className="font-semibold">Genre:</span> {game.genre}
+            </p>
+            <p>
+              <span className="font-semibold">Developer:</span> {game.developer}
+            </p>
+            <p>
+              <span className="font-semibold">Release:</span> {game.release}
+            </p>
+            <p>
+              <span className="font-semibold">Metacritic:</span>{" "}
+              {game.metacritic}
+            </p>
+          </div>
+        ))}
+      </div>
       <div className="hidden mx-4 mb-10 p-4 pb-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8 md:p-8 lg:p-12">
         <h2 className="text-3xl text-center font-bold underline my-4">
           Confirmed Games
