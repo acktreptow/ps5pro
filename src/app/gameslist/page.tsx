691aefac0ca02127c56248fb160f2994bf2648f2
acktreptow <acktreptow@gmail.com>
2024-11-05 07:35:52 +0000

    create/implement get unconfirmed games function

--- src/app/gameslist/page.tsx
+++ src/app/gameslist/page.tsx
@@ -68,9 +68,36 @@ async function getDetailedGames() {
   }
 }
 
+async function getUnconfirmedGames() {
+  try {
+    const unconfirmedGames = await prisma.unconfirmedGame.findMany({
+      where: { published: true },
+      select: {
+        id: true,
+        title: true,
+        genre: true,
+        developer: true,
+        psStudios: true,
+        psvr2: true,
+        release: true,
+        metacritic: true,
+      },
+      orderBy: {
+        title: "asc",
+      },
+    });
+    console.log("Unconfirmed games fetched successfully:", unconfirmedGames);
+    return unconfirmedGames;
+  } catch (error) {
+    console.error("Error fetching unconfirmed games:", error);
+    throw error;
+  }
+}
+
 const GamesListPage = async () => {
   const confirmedGames = await getConfirmedGames();
   const detailedGames = await getDetailedGames();
+  const unconfirmedGames = await getUnconfirmedGames();
   return (
     <div className="container mx-auto flex-grow text-slate-800">
       <h1 className="text-center text-4xl font-extrabold mt-8 mb-12 mx-4 p-8 bg-slate-100 border-2 rounded shadow-lg md:mx-8">
@@ -158,6 +185,40 @@ const GamesListPage = async () => {
         <span className="font-semibold">Total Confirmed Games:</span>{" "}
         {confirmedGames.length}
       </h4>
+      <h2 className="text-3xl text-center font-bold underline mb-4 lg:text-start lg:mx-8">
+        9 Games That Most Need a PS5 Pro Upgrade
+      </h2>
+      <div className="text-xl text-center mb-8 mx-4 grid gap-5 md:text-lg md:mx-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-4">
+        {unconfirmedGames.map((game) => (
+          <div
+            className="py-6 px-4 bg-slate-100 border-2 rounded shadow-md lg:px-2"
+            key={game.id}
+          >
+            <h3 className="font-extrabold text-2xl mb-2 md:text-xl lg:mb-4">
+              {game.title}
+            </h3>
+            <p>
+              <span className="font-semibold">Genre:</span> {game.genre}
+            </p>
+            <p>
+              <span className="font-semibold">Developer:</span> {game.developer}
+            </p>
+            <p>
+              <span className="font-semibold">Release:</span> {game.release}
+            </p>
+            <p>
+              <span className="font-semibold">Metacritic:</span>{" "}
+              {game.metacritic}
+            </p>
+            {game.psStudios && (
+              <p className="text-start font-semibold mx-8">#PS Studios</p>
+            )}
+            {game.psvr2 && (
+              <p className="text-start font-semibold mx-8">#PSVR2</p>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
