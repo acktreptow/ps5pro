a3d282a628ec4c627adbc0a2961f1138cfbd94f9
acktreptow <acktreptow@gmail.com>
2025-01-22 04:29:50 +0000

    remove uneccessary divs. add classnames for clarity

--- src/app/graphicsvsperformance/page.tsx
+++ src/app/graphicsvsperformance/page.tsx
@@ -87,61 +87,53 @@ const GraphicsvsPerformancePage = (): JSX.Element => {
               <li>Does the game have a long draw distance?</li>
             </ul>
           </div>
-          <div>
-            <div>
-              <h3 className="text-center text-2xl font-bold underline mb-2 md:text-start lg:ml-8">
-                Settings factors
-              </h3>
-              <ul className="list-disc mb-8 ml-4 space-y-1 lg:ml-12">
-                <li>Does the game have an FOV slider?</li>
-                <li>Does the game have good gamma calibration?</li>
-                <li>Can you toggle motion blur?</li>
-                <li>Can you toggle chromatic aberration?</li>
-                <li>Can you toggle depth of field?</li>
-                <li>Can you toggle film grain?</li>
-                <li>Can you toggle VSync?</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-center text-2xl font-bold underline mb-2 md:text-start lg:ml-8">
-                Screen factors
-              </h3>
-              <ul className="list-disc mb-8 ml-4 space-y-1 lg:ml-12">
-                <li>What size is the screen?</li>
-                <li>What resolution is the screen?</li>
-                <li>Is the screen a TV, monitor, laptop, phone etc?</li>
-                <li>Is it an OLED screen or an LCD/LED/QLED?</li>
-                <li>Does the screen support HDR?</li>
-                <li>What is the max refresh rate of the screen?</li>
-                <li>Does the screen support a Game mode?</li>
-                <li>Does the screen have a low response time?</li>
-                <li>Does the screen support VRR?</li>
-                <li>Does the screen use other HDMI 2.1 features?</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-center text-2xl font-bold underline mb-2 md:text-start lg:ml-8">
-                External factors
-              </h3>
-              <ul className="list-disc mb-8 ml-4 space-y-1 lg:ml-12">
-                <li>How close are you to the screen?</li>
-                <li>Do you play in a dark or bright room?</li>
-                <li>Do you have others around you when playing?</li>
-                <li>How long are your gaming sessions?</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-center text-2xl font-bold underline mb-2 md:text-start lg:ml-8">
-                Personal factors
-              </h3>
-              <ul className="list-disc ml-4 space-y-1 lg:ml-12">
-                <li>Can you feel a big gap between 30fps & 60fps?</li>
-                <li>Can you feel a big gap between resolutions?</li>
-                <li>What&#39;s your skill level in singleplayer games?</li>
-                <li>Need extra frames to compete in multiplayer?</li>
-                <li>Do you use photo mode a lot?</li>
-              </ul>
-            </div>
+          <div className="lg:w-1/2">
+            <h3 className="text-center text-2xl font-bold underline mb-2 md:text-start lg:ml-8">
+              Settings factors
+            </h3>
+            <ul className="list-disc mb-8 ml-4 space-y-1 lg:ml-12">
+              <li>Does the game have an FOV slider?</li>
+              <li>Does the game have good gamma calibration?</li>
+              <li>Can you toggle motion blur?</li>
+              <li>Can you toggle chromatic aberration?</li>
+              <li>Can you toggle depth of field?</li>
+              <li>Can you toggle film grain?</li>
+              <li>Can you toggle VSync?</li>
+            </ul>
+            <h3 className="text-center text-2xl font-bold underline mb-2 md:text-start lg:ml-8">
+              Screen factors
+            </h3>
+            <ul className="list-disc mb-8 ml-4 space-y-1 lg:ml-12">
+              <li>What size is the screen?</li>
+              <li>What resolution is the screen?</li>
+              <li>Is the screen a TV, monitor, laptop, phone etc?</li>
+              <li>Is it an OLED screen or an LCD/LED/QLED?</li>
+              <li>Does the screen support HDR?</li>
+              <li>What is the max refresh rate of the screen?</li>
+              <li>Does the screen support a Game mode?</li>
+              <li>Does the screen have a low response time?</li>
+              <li>Does the screen support VRR?</li>
+              <li>Does the screen use other HDMI 2.1 features?</li>
+            </ul>
+            <h3 className="text-center text-2xl font-bold underline mb-2 md:text-start lg:ml-8">
+              External factors
+            </h3>
+            <ul className="list-disc mb-8 ml-4 space-y-1 lg:ml-12">
+              <li>How close are you to the screen?</li>
+              <li>Do you play in a dark or bright room?</li>
+              <li>Do you have others around you when playing?</li>
+              <li>How long are your gaming sessions?</li>
+            </ul>
+            <h3 className="text-center text-2xl font-bold underline mb-2 md:text-start lg:ml-8">
+              Personal factors
+            </h3>
+            <ul className="list-disc ml-4 space-y-1 lg:ml-12">
+              <li>Can you feel a big gap between 30fps & 60fps?</li>
+              <li>Can you feel a big gap between resolutions?</li>
+              <li>What&#39;s your skill level in singleplayer games?</li>
+              <li>Need extra frames to compete in multiplayer?</li>
+              <li>Do you use photo mode a lot?</li>
+            </ul>
           </div>
         </div>
       </div>
